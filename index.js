dc903cd42f25164d7239bf06034c784aa19ff99e
Hieu <xuanhieu0012@gmail.com>
2021-08-15 21:40:07 -0500

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', () =>{
+    let p = document.querySelector('#text')
+    p.textContent = "This is really cool!"
+    console.log()
+})
